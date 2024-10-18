dbce7b43ce69e7ea76d7f349f4d49169b9240b7e
wodud6967 <wodud6967@gmail.com>
2024-10-18 14:53:28 +0900

    통합테스트

--- src/main/java/org/example/springv3/core/config/FilterConfig.java
+++ src/main/java/org/example/springv3/core/config/FilterConfig.java
@@ -1,6 +1,7 @@
 package org.example.springv3.core.config;
 
 import jakarta.servlet.FilterRegistration;
+import org.example.springv3.core.filter.CorsFilter;
 import org.example.springv3.core.filter.JwtAuthorizationFilter;
 import org.example.springv3.user.User;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -15,11 +16,20 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     @Bean
+    public FilterRegistrationBean<?> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0);
+        return bean;
+    }
+
+    @Bean
     public FilterRegistrationBean<?> jwtAuthorizationFilter(){
         FilterRegistrationBean<JwtAuthorizationFilter> bean
                 = new FilterRegistrationBean<>(new JwtAuthorizationFilter());
         bean.addUrlPatterns("/api/*");
-        bean.setOrder(0);
+        bean.setOrder(1);
         return bean;
     }
 }
